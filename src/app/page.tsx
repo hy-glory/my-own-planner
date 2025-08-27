dc20873ea08d5abf5181d46a0392c649cbec4f68
hykim <joywithjoy1203@gmail.com>
2025-08-27 13:55:40 +0900

    fixed-note 버그 잡는 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
-'use client';
-import { useState } from 'react';
-import Header from './Header';
-import DailyNote from './DailyNote';
-import FixedNote from './FixedNote';
+"use client";
+import { useState } from "react";
+import Header from "./Header";
+import DailyNote from "./DailyNote";
+import { NoteType, FixedNote } from "./FixedNote";
 
 const Home = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [memos, setMemos] = useState<FixedNote[]>([]);
+
+  const addNote = () => {
+    const newNote: FixedNote = {
+      id: Date.now(),
+      text: "",
+    };
+    setMemos((prev) => [...prev, newNote]);
+  };
+
+  const updateNote = (id: number, text: string, title?: string) => {
+    setMemos((prev) =>
+      prev.map((m) => (m.id === id ? { ...m, text, title } : m))
+    );
+  };
+
+  const deleteNote = (id: number) => {
+    setMemos((prev) => prev.filter((note) => note.id !== id));
+  };
 
   return (
     <div>
       <Header
         selectedDate={selectedDate}
         onSelectDate={setSelectedDate}
+        onAddNote={addNote}
       />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <DailyNote selectedDate={selectedDate} />
-        <FixedNote />
+        <div className="p-4 flex flex-wrap gap-4">
+          {memos.map((note) => (
+            <FixedNote
+              key={note.id}
+              note={note}
+              onChange={updateNote}
+              onDelete={deleteNote}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
